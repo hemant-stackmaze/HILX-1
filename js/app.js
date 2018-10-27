bc7d4d691a23c6c7dd78da8fbbea0f4fd5b1e89e
patelmr3 <patelmr3@hotmail.com>
2018-10-27 20:12:38 +0530

    Home collection slider updates

--- js/app.js
+++ js/app.js
@@ -1,6 +1,8 @@
 $(document).foundation();
 
 $(document).ready(function() {
+
+  var winHeight = $(window).height();
   
   //best seller slider
   $('.best-sellers-slider').slick({
@@ -31,6 +33,54 @@ $(document).ready(function() {
         }
       }
     ]
+  });
+
+  //collections slider
+  var collections = $('.collections');
+  var collectionHeight = collections.height();
+  var collectionsTop = collections.offset().top;
+  var collectionsBottom = collectionsTop + collectionHeight;
+
+  $('.collection').height(winHeight);
+  console.log(collectionsTop);
+
+
+  $(window).scroll(function() {
+    var winScrollTop = $(this).scrollTop();
+
+    if(winScrollTop >= collectionsTop) {
+      $('.collections').addClass('isActive');
+      console.log(1);
+    } else {
+      $('.collections').removeClass('isActive');
+      console.log(collectionsTop);
+      console.log(winScrollTop);
+    }
+
+    $('.collection').each(function() {
+      var collection = $(this);      
+      
+      if(winScrollTop >= collection.offset().top && !collection.hasClass('isActive')) {        
+        var collectionIndex = collection.data('collection-index');
+        $('.collection').removeClass('isActive');
+        collection.addClass('isActive');
+
+        $('.item').removeClass('isActive');
+        var navItem = $('.item[data-collection-nav-index="'+ collectionIndex +'"]');
+
+        if(!navItem.hasClass('isActive')) {
+          navItem.addClass('isActive');
+        }
+        
+      } 
+    });
+
+    //check if collections is in viewport
+    if (winScrollTop >= collectionsTop && winScrollTop <= collectionsBottom) {
+      //console.log('Collection in view port');
+    } else {
+      //console.log('Collection outside view port');
+    }
   });
 
 });
